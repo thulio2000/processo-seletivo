6acb999cf5e2113ae848bb1b316c8dcb814e6caa
Túlio <lucastuliolima@gmail.com>
2023-09-28 15:17:49 -0300

    Selecionando candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,7 +1,40 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		System.out.println("Processo seletivo");
+		selecaoCandidatos();
+	}
+	static void selecaoCandidatos() {
+		String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge" };
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = ValorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido );
+			if (salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+				candidatosSelecionados++;
+			}
+			candidatoAtual++;
+		}
+	}
+	static void analisarCandidato(double salarioPretendido) {
+		double salarioBase = 2000.0;
+		if(salarioBase >= salarioPretendido) {
+			System.out.println("Ligar para o candidato.");
+		} else if(salarioBase == salarioPretendido) {
+			System.out.println("Ligar para o candidato com contra proposta.");
+		} else {
+			System.out.println("Aguardando o resultado dos demais candidatos.");
+		}
+		
+	}
+	static double ValorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 }
